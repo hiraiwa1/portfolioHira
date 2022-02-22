e3614da60c342e825f33f5c3872dee9910997d26
hiraiwa1 <ichi.cun.sky.high@gmail.com>
2022-02-22 11:19:20 +0900

    distの出力

--- pages/workProduct.vue
+++ pages/workProduct.vue
@@ -60,37 +60,6 @@
         <!-- /.works__item -->
         <div class="works__item slide">
           <a
-            href="http://xs330034.xsrv.jp/"
-            class="works__img-link"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              class="works__img img-blur"
-              src="~/static/images/works/work-03.png"
-              alt=""
-            />
-          </a>
-          <div class="works__box">
-            <p class="works__item-title">
-              <a
-                class="works__item-title--link"
-                href="https://twitter.com/showheyohtaki"
-                >デイトラのショーヘーさん</a
-              >のデザインカンプ
-            </p>
-            <a
-              class="works__item-link"
-              href="https://github.com/hiraiwa1/md2020"
-              target="_blank"
-              rel="noopener noreferrer"
-              ><i class="fab fa-github-square"></i
-            ></a>
-          </div>
-        </div>
-        <!-- /.works__item -->
-        <div class="works__item slide">
-          <a
             href="https://moveheart.netlify.app/"
             class="works__img-link"
             target="_blank"
@@ -117,24 +86,6 @@
               rel="noopener noreferrer"
               ><i class="fab fa-github-square"></i
             ></a>
-          </div>
-        </div>
-        <!-- /.works__item -->
-        <div class="works__item slide">
-          <a
-            href="https://dev-www3.nhk.or.jp/news/special/practice_hiraiwa/nyanco/master/"
-            class="works__img-link"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              class="works__img"
-              src="~/static/images/works/work-05.png"
-              alt=""
-            />
-          </a>
-          <div class="works__box">
-            <p class="works__item-title">にゃんこ課題</p>
           </div>
         </div>
         <!-- /.works__item -->
