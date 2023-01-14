ffca09830e60e00eb11ca9fd61b3ea511dd53b60
hiraiwa1 <ichi.cun.sky.high@gmail.com>
2023-01-15 03:05:02 +0900

    作品プラス

--- pages/workProduct.vue
+++ pages/workProduct.vue
@@ -5,6 +5,7 @@
       <p class="section__text works__title-text">
         XDやPhotoShopのデザインカンプからの練習ーコーディングや自身でデザインからコーディングまでやった作品です。
       </p>
+      <p class="section__text">順：古→新</p>
     </div>
     <div class="works-content">
       <div class="section__inner works__inner">
@@ -40,7 +41,7 @@
             />
           </a>
           <div class="works__box">
-            <p class="works__item-title">コーディングからデザインまで自作</p>
+            <p class="works__item-title">デザインからコーディングまで自作</p>
           </div>
         </div>
         <!-- /.works__item -->
@@ -82,7 +83,25 @@
             />
           </a>
           <div class="works__box">
-            <p class="works__item-title">コーディングから自作</p>
+            <p class="works__item-title">【Three.js】コーディングから自作</p>
+          </div>
+        </div>
+        <!-- /.works__item -->
+        <div class="works__item slide">
+          <a
+            href="https://spectacular-naiad-1a5604.netlify.app/"
+            class="works__img-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              class="works__img"
+              src="~/static/images/works/work-07.jpg"
+              alt=""
+            />
+          </a>
+          <div class="works__box">
+            <p class="works__item-title">【Vue.js, Three.js】コーディングから自作</p>
           </div>
         </div>
         <!-- /.works__item -->
